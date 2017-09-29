591af18b9edba5b58c7eb6f7ff5b51a397f1cc22
wldn <wildansnahar@gmail.com>
2017-09-29 15:12:50 +0700

    add usage.sh

--- usage.sh
+++ usage.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo 'Usage : '
-echo '1) delete.sh => '
+echo '1) read.sh    => ./read.sh'
+echo '2) create.sh  => sudo ./create.sh [domain]'
+echo '3) delete.sh  => sudo ./delete.sh [domain]'
+echo '4) update.sh  => sudo ./update.sh [old-domain] [new-domain]'
+
 
